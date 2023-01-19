226bec17b366a80a31fd59dac8f76700852683f7
Muk007 <38641966+Muk007@users.noreply.github.com>
2023-01-19 14:07:38 +0530

    Update install-influx_v1.sh

--- install-influx_v1.sh
+++ install-influx_v1.sh
@@ -8,12 +8,12 @@ key="23a1c8836f0afc5ed24e0486339d7cc8f6790b83886c4c96995b88a061c5bb5d"
 key_path="/etc/apt/trusted.gpg.d/influxdb.gpg"
 influx_debian_url="https://repos.influxdata.com/debian"
 
-# Commands to install influxdb
+# Commands to install influxdb and telegraf
 curl -s ${key_url} > influxdb.key
 echo "${key} influxdb.key" | sha256sum -c && cat influxdb.key | gpg --dearmor | sudo tee ${key_path} > /dev/null
 echo "deb [signed-by=${key_path}] ${influx_debian_url} stable main" | sudo tee /etc/apt/sources.list.d/influxdata.list
 
-sudo apt-get update && sudo apt-get install influxdb
+sudo apt-get update && sudo apt-get install influxdb telegraf
 # If you want to install influxdb_v2, replace influxdb (line 16) with influxdb2
 
 sudo service influxdb start
