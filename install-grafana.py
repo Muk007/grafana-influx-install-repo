e5ee85ecea0a14089d4c466f0e174fd1f508b89e
Muk007 <38641966+Muk007@users.noreply.github.com>
2023-01-19 13:42:40 +0530

    Add files via upload

---
+++ install-grafana.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import json
+import logging
+
+logging.basicConfig(filename='/var/log/grafana/grafana-install.log', format='%(asctime)s, %(msecs)d %(name)s %(levelname)s %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+
+def load_config (config_file_path):
+    config_data = {}
+    with open(config_file_path) as config_file:
+        config_data = json.load(config_file)
+    return config_data
+
+def download_binary(url, grafana_home):
+    cmd = "sudo apt update -y  \ncurl -o "+grafana_home+"grafana-9.3.2.tar.gz "+url
+    os.system(cmd)
+    logging.info("System updated & grafana binary downloaded.")
+
+def grafana_config(grafana_home):
+    cmd = "tar -xvf "+grafana_home+"grafana-9.3.2.tar.gz -C "+grafana_home+" \nmv "+grafana_home+"grafana-9.3.2 "+grafana_home+"grafana  \nsudo cp -R "+grafana_home+"grafana/bin/grafana-server /usr/local/bin/grafana-server  \nsudo chmod +x /usr/local/bin/grafana-server"
+    os.system(cmd)
+    logging.info("grafana-server binary configured.")
+     
+def start_script(service_name, grafana_home):
+    with open("/etc/systemd/system/grafana.service", "w") as file:
+        lines = ["[Unit]\n", "Description=Grafana Service\n", "Documentation=https://grafana.com/docs/grafana/latest/\n\n", "[Service]\n","ExecStart=grafana-server -homepath "+grafana_home+"grafana\n\n", "[Install]\n", "WantedBy=multi-user.target\n", "Alias=grafana.service\n"]
+        file.writelines(lines)
+        file.close()
+    cmd = "sudo systemctl start "+service_name
+    os.system(cmd)
+    logging.info("Grafana started...")
+    
+config_data = {}
+
+try:
+    config_data = load_config(sys.argv[1])
+    download_binary(config_data['url'], config_data['grafana_home']) 
+    grafana_config(config_data['grafana_home'])
+    start_script(config_data['service_name'], config_data['grafana_home'])
+    
+except Exception as exp:
+    logging.exception("Grafana installation failed.")
---
+++ install-influx_v1.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+exec > /var/log/influxdb-install.log 2>&1
+
+# Declaring variables
+key_url="https://repos.influxdata.com/influxdb.key"
+key="23a1c8836f0afc5ed24e0486339d7cc8f6790b83886c4c96995b88a061c5bb5d"
+key_path="/etc/apt/trusted.gpg.d/influxdb.gpg"
+influx_debian_url="https://repos.influxdata.com/debian"
+
+# Commands to install influxdb
+curl -s ${key_url} > influxdb.key
+echo "${key} influxdb.key" | sha256sum -c && cat influxdb.key | gpg --dearmor | sudo tee ${key_path} > /dev/null
+echo "deb [signed-by=${key_path}] ${influx_debian_url} stable main" | sudo tee /etc/apt/sources.list.d/influxdata.list
+
+sudo apt-get update && sudo apt-get install influxdb
+sudo service influxdb start
+
+
+# NOTE: Make sure to enable authentication and create user/paswd in influx which will be used while making influx as data source for the grafana. 
